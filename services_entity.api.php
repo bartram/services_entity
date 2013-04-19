f1692bd53a6eb84f0c0ca6d90f935c2a5d977449
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-04-19 14:20:44 +0100

    by joachim: Fixed api.php docs.

--- services_entity.api.php
+++ services_entity.api.php
@@ -11,19 +11,18 @@
  */
 
 /**
- * Identifies a server implementation to Services.
+ * Defines Services Entity resource plugins.
  *
  * @return
- *   An associative array with the following keys.
- *
- *   - title: The display name of the controller.
- *	 - description: A short description of the controller.
- *   - class: the class name of the controller. This should be in a separate file
- *     included in the module.info file.
+ *   An associative array with the following keys:
+ *   - 'title': The display name of the controller.
+ *	 - 'description': A short description of the controller.
+ *   - 'class': The class name of the controller. This should be in a separate
+ *     file included in the module .info file.
  */
 function hook_services_entity_resource_info() {
   $result = array();
-  
+
   $result['generic'] = array(
     'title' => 'Generic Entity Processor',
     'description' => 'Acts as a generic wrapper for entities. Data structures are exactly what they are in Drupal.',
